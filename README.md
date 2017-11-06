b392a9c5989e96feaf2ed3a0c38db26fbf25b5e9
Tristan Gläs <tristan.glaes@gmx.de>
2017-11-06 23:45:02 +0100

    Setup 1. und 2. Projekt

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# team5ds
+﻿# team5ds
 
 Gitrepository für alle drei Projekte.
+
+C Installation für Netbeans https://www.sololearn.com/Discuss/341378/how-to-install-compiler-debugger-for-netbeans-8-2-via-cygwin
---
+++ project1/file_query/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Tristan
+ *
+ * Created on 6. November 2017, 23:35
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Test");
+    return (EXIT_SUCCESS);
+}
+
