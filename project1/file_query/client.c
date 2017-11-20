99b20da05ec65519ed9fee8e0ecdee47c56ac0a6
zsisamci <zsisamci@Lenovo-PC.lan>
2017-11-20 23:47:18 +0100

    change client to posix socks and fix

--- project1/file_query/client.c
+++ project1/file_query/client.c
@@ -1,20 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <winsock2.h>
-#include <winsock.h>
+#include <string.h> 
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #include "data.h"
+#include "client.h"
 
-int get_data_from_user(char FAR* data);
 
 int run_client(char* ip_adress, int port) {
-    
+
     // Variabeln für den Socket.
-    SOCKET sock;
+    int sock;
     struct sockaddr_in client;
-    
+    struct in_addr ipaddr;
+    socklen_t addr_size;
     // Variabel für das Datenpacket, dass an den Server geschickt wird.
-    char FAR* data = malloc(MAX_DATA_SIZE);
-    
+    char* data = malloc(MAX_DATA_SIZE);
+
     /* 
      * Bereinigen des Speichers und setzten der Parameter
      * AF_INET -> IPv4
@@ -22,34 +28,49 @@ int run_client(char* ip_adress, int port) {
      * port -> Der Port auf dem der Server horcht.
      * 
      */
+
+
+    //load and format  ipadress  exit if wrng format
+    if (inet_aton(ip_adress, &ipaddr) == 0) {
+        perror("Client Error wrong ip Adress format");
+        return -1;
+    }
+    //set memory of  sockaddr_in struct to null
     memset(&client, 0, sizeof (client));
     client.sin_family = AF_INET;
-    client.sin_addr.s_addr = inet_addr(ip_adress);
+    client.sin_addr = ipaddr;
     client.sin_port = htons(port);
-    
+
     // Erstellen eines TCP Sockets.
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Client Error");
         return -1;
     }
-    
+
     // Versucht mit dem Server eine Verbindung einzugehen.
     if (connect(sock, (struct sockaddr*) &client, sizeof (client)) < 0) {
         perror("Client Error");
+        close(sock);
         return -1;
     }
-    
+
     // Liest die 5 Dateinamen und die Anzahl der Bytes des Clienten ein.
     if (get_data_from_user(data) < 0) {
-        printf("Client Error while receiving user data!\n");
+        printf("Client Error while receiving user data from stdin!\n");
         return -1;
     }
-    
+
     // Sendet die Daten an den Server.
-    send(sock, data, MAX_DATA_SIZE, 0);
+    send(sock, data, MAX_DATA_SIZE, MSG_NOSIGNAL);
+    
+    send_data(sock,data);
+
+
+    close(sock);
     return 0;
 }
+
 /*
  * Liest 5 Dateinamen und eine Zahl ein, die getrennt mit Newline in
  * data gespeichert werden.
@@ -57,12 +78,16 @@ int run_client(char* ip_adress, int port) {
  * Data sieht nach der Eingabe des Users etwa so aus:
  * test.txt\ntest2.txt\ntest3.txt\ntest4.txt\nfile.lib\n1000\n ...
  * 
+ *  muss verbesser werden falls man filenamen groesser MAX_FILE_NAME_SIZE eingibt 
+ * wird beim nachsten fgets aufruft die ubrigen zeichen eingelsen statt ein neues
+ *  
+ * 
  */
-int get_data_from_user(char FAR* data) {
-    
+int get_data_from_user(char* data) {
+
     char* file_name = malloc(MAX_FILE_NAME_SIZE);
     char* number_of_bytes = malloc(5);
-    
+
     printf("Enter the first filename : ");
     fgets(file_name, MAX_FILE_NAME_SIZE, stdin);
     strncat(data, file_name, MAX_FILE_NAME_SIZE);
@@ -81,5 +106,13 @@ int get_data_from_user(char FAR* data) {
     printf("Enter a number of bytes  : ");
     fgets(number_of_bytes, MAX_FILE_NAME_SIZE, stdin);
     strncat(data, number_of_bytes, 5);
+    return 0;
+}
+
+int send_data(int sock, char* data) {
+
+
+
+
     return 0;
 }
