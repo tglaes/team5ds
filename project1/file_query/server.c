77cac3231794b181b3a1a3648e254304326cc693
zsisamci <zsisamci@Lenovo-PC.lan>
2017-11-21 21:23:58 +0100

    change server to posix socks, added loop to accept multiple connection

--- project1/file_query/server.c
+++ project1/file_query/server.c
@@ -1,14 +1,18 @@
-#include <winsock2.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <winsock.h>
 #include <string.h>
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #include "file.h"
 #include "data.h"
 #include "server.h"
 
 
-int get_data_from_client_package(char FAR* data, char file_names[][64], char** number_of_bytes);
+int get_data_from_client_package(char* data, char file_names[][64], char** number_of_bytes);
 
 /*
  * Startet einen Server, der mehrere Anfragen von Clienten gleichzeitig
@@ -16,19 +20,19 @@ int get_data_from_client_package(char FAR* data, char file_names[][64], char** n
  * Verbindungen horchen soll. Der Server benutzt IPv4 und TCP.
  */
 int run_server(int port) {
-    
-    
+
+
     // Variabeln für den Socket.
-    SOCKET sock, sock2;
-    struct sockaddr_in server;
-    unsigned sock_len;
-    
+    int sock, new_sock, pid;
+    struct sockaddr_in server, client;
+    socklen_t sock_len;
+
     // Variabeln für den Datenaustausch und Datenverarbeitung.
-    char FAR* data = malloc(MAX_DATA_SIZE);
-    char file_names[5][64];
+    char* data = malloc(MAX_DATA_SIZE);
+    char file_names[MAX_FILE_TO_ACCEPT][MAX_FILE_NAME_SIZE];
     char* number_of_bytes_string;
     int number_of_bytes = 0;
-    
+
     /*
      *  Bereinigen des Speichers des Servers und setzten der Parameter:
      * AF_INET -> IPv4
@@ -40,69 +44,85 @@ int run_server(int port) {
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(port);
-    
+
     // Erstellen des TCP Sockets.
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Server Error");
+        free(data);
         return -1;
     }
-    
+
     // Binden des Sockets.
     if (bind(sock, (struct sockaddr*) &server, sizeof (server)) < 0) {
         perror("Server Error");
+        close(sock);
+        free(data);
         return -1;
     }
-    
+
     // Sag dem Server, dass er auf einkommende Verbindungen horchen soll.
     if (listen(sock, 5) < 0) {
         perror("Server Error");
+        close(sock);
+        free(data);
         return -1;
     }
-    
-    // Erstellt einen zweiten Socket, der dann mit einem Cienten verbunden ist.
-    sock_len = sizeof (server);
-    sock2 = accept(sock, (struct sockaddr*) &server, &sock_len);
-    if (sock2 < 0) {
-        perror("Server Error");
-        return -1;
+
+
+    //schleife die 
+    sock_len = sizeof (client);
+    while (1) {
+        new_sock = accept(sock, (struct sockaddr*) &client, &sock_len);
+        if (new_sock < 0) {
+            //TODO check if error came because client abborted connection then 
+            // continue otherwise call perror and return -1
+            fprintf(stderr, "%s", "accepting error continue\n");
+            continue;
+        }
+         
+       
+
     }
-    
-    // Empfängt ein Datenpacket des Clienten.
-    if (recv(sock2, data, MAX_DATA_SIZE, MSG_WAITALL) < 0) {
-        perror("Server Error");
-        return -1;
-    }
-    
-     printf(data);
-    
-//    // Sortiert die Daten des Packets in die Dateinamen und die Anzahl der Bytes.
-//    get_data_from_client_package(data, file_names, &number_of_bytes_string);
-//    
-//    /*
-//     * Wenn die Anzahl der Bytes kleiner ist als die Anzahl der Bytes für
-//     * "Datei nicht gefunden!", dann legen wir die Anzahl der Bytes auf 22.
-//     */
-//    number_of_bytes = atoi(number_of_bytes_string);
-//    if(number_of_bytes < 21){
-//        number_of_bytes = 21;
-//    }
-//    
-//    // Allokierte Speicher für die Bytes plus jeweils ein Nullbyte.
-//    char* bytes_from_file = malloc(5 * (number_of_bytes + 1));
-//
-//    // Lese die Bytes aus den Dateinen und speichere sie in bytes_from_file.
-//    get_bytes_from_file(&bytes_from_file , file_names, number_of_bytes);
-//    
-//    
-//   // TODO: Schicke Client eine Antwort und Speicher freigeben.
-//   // TODO: Einen neuen Prozess für jede einkommende Verbindung erstellen. 
-//    for(int i= 0; i < sizeof(bytes_from_file); i++){
-//        printf("%c", bytes_from_file[i]);
-//    }
-//    
-//    
-//    //closesocket(sock);
+
+
+
+
+    //    // Empfängt ein Datenpacket des Clienten.
+    //    if (recv(new_sock, data, MAX_DATA_SIZE, MSG_WAITALL) < 0) {
+    //        perror("Server Error");
+    //        return -1;
+    //    }
+
+    //    printf(data);
+
+    //    // Sortiert die Daten des Packets in die Dateinamen und die Anzahl der Bytes.
+    //    get_data_from_client_package(data, file_names, &number_of_bytes_string);
+    //    
+    //    /*
+    //     * Wenn die Anzahl der Bytes kleiner ist als die Anzahl der Bytes für
+    //     * "Datei nicht gefunden!", dann legen wir die Anzahl der Bytes auf 22.
+    //     */
+    //    number_of_bytes = atoi(number_of_bytes_string);
+    //    if(number_of_bytes < 21){
+    //        number_of_bytes = 21;
+    //    }
+    //    
+    //    // Allokierte Speicher für die Bytes plus jeweils ein Nullbyte.
+    //    char* bytes_from_file = malloc(5 * (number_of_bytes + 1));
+    //
+    //    // Lese die Bytes aus den Dateinen und speichere sie in bytes_from_file.
+    //    get_bytes_from_file(&bytes_from_file , file_names, number_of_bytes);
+    //    
+    //    
+    //   // TODO: Schicke Client eine Antwort und Speicher freigeben.
+    //   // TODO: Einen neuen Prozess für jede einkommende Verbindung erstellen. 
+    //    for(int i= 0; i < sizeof(bytes_from_file); i++){
+    //        printf("%c", bytes_from_file[i]);
+    //    }
+    //    
+    //    
+    //    //closesocket(sock);
     return 0;
 }
 
@@ -118,8 +138,8 @@ int run_server(int port) {
  * test.txt\ntest2.txt\ntest3.txt\ntest4.txt\ntest5.txt\n1000\n ...
  * 
  */
-int get_data_from_client_package(char FAR* data, char file_names[][64], char** number_of_bytes) {
-    
+int get_data_from_client_package(char* data, char file_names[][64], char** number_of_bytes) {
+
     printf(data);
     char delimiter[] = "\n";
     // Splitte den data String und kopiere das Ergebnis in file_names.
