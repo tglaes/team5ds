e0cd19eb347198e2f242f7878b546286c5c313de
juriPI <Juri-Bevz.94@gmx.de>
2017-12-16 20:00:18 +0100

    Update ServerDialog.java
    kommi added

--- project2/PrimeNumbers/src/Main/ServerDialog.java
+++ project2/PrimeNumbers/src/Main/ServerDialog.java
@@ -1,38 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Main;
 
 import static Main.ClientDialog.END;
 import java.util.Scanner;
 
 /**
- *
- * @author Tristan Glaes
+ * Hilfsklasse zum erstellen des Dialogs fuer den Server
+ * 
+ *@author Tristan Glaes,Meris Krupic,Jurie Golovencic,Vadim Khablov 
+ *@version 14.12.2017
  */
 public class ServerDialog {
     
     public static final int START_SERVER           = 1;
     public static final int END                    = 0; 
-    private static Scanner input = new Scanner(System.in); 
+    private static Scanner input = new Scanner(System.in);
+    
     /**
-     * Liest eingegebene Int, String.
+     * Liest eingegebene String und gibt diesen als int zurueck.
      */
     public static int readlnInt(String stringInput) {
         System.out.print(stringInput);
         String line = input.nextLine();
         return Integer.parseInt(line);
     }
-
+    
+    /**
+    * Liest String ein.
+    */
     public static String readlnString(String stringInput) {
         System.out.print(stringInput);
         String word = input.nextLine();
         return word;
     }
     /**
-     * Menue anzeigen und functionen einlesen
+     * Menue anzeigen und Funktionen einlesen
      */
     public static int readFunction() { 
         System.out.println ( 
