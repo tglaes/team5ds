dc28f48b89241b8084cd382ed8100d8632c2fa24
Tristan Gläs <tristan.glaes@gmx.de>
2017-12-17 21:41:15 +0100

    Projekt 3 einige Änderungen

--- project3/DistributedBoards/src/main/java/test/Test2.java
+++ project3/DistributedBoards/src/main/java/test/Test2.java
@@ -1,9 +1,5 @@
 package test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,10 +7,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("/test")
-public class Test {
+@Path("/test2")
+public class Test2 {
 
-	/*
+	
 	// This method is called if TEXT_PLAIN is request
 	  @GET
 	  @Produces(MediaType.TEXT_PLAIN)
@@ -27,13 +23,14 @@ public class Test {
 	  @Produces(MediaType.TEXT_XML)
 	  public String sayXMLHello() {
 	    return "<?xml version=\"1.0\"?>" + "<hello> Test" + "</hello>";
-	  }*/
+	  }
 
 	  // This method is called if HTML is request
 	  @GET
 	  @Produces(MediaType.TEXT_HTML)
-	  public String sayHtmlHello() throws IOException {
-		  
-		 return new String(Files.readAllBytes(Paths.get("C:\\Users\\Tristan Glaes\\Documents\\team5ds\\project3\\DistributedBoards\\HTML\\test.html")));
+	  public String sayHtmlHello() {
+	    return "<html><head><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css\" integrity=\"sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb\" crossorigin=\"anonymous\">\r\n" + 
+	    		"<head> " + "<title>" + "Test" + "</title>"
+	        + "<body><h2>This is a test</h2>" + "</html> ";
 	  }
 }
