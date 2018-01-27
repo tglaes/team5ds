976f0b7f07dff353523badbdcea26bd7957e44ae
Tristan Gläs <tristan.glaes@gmx.de>
2018-01-27 17:34:45 +0100

    Registrieren und Resourcen

--- project3/DistributedBoards/src/main/java/routes/Registration.java
+++ project3/DistributedBoards/src/main/java/routes/Registration.java
@@ -1,8 +1,17 @@
 package routes;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/Registration")
 public class Registration {
 
+	@GET
+	public InputStream sendRegistrationPage() throws FileNotFoundException {
+		return Resources.getResource("Registrieren.html", "html");
+	}
+	
 }
--- project3/DistributedBoards/src/main/java/routes/Resources.java
+++ project3/DistributedBoards/src/main/java/routes/Resources.java
@@ -13,29 +13,39 @@ import javax.ws.rs.QueryParam;
 public class Resources {
 	
 	@GET
-	public InputStream returnResource(@QueryParam("resourceName") String name, @QueryParam("resourceType") String type) throws FileNotFoundException {
+	public static InputStream getResource(@QueryParam("resourceName") String name, @QueryParam("resourceType") String type) throws FileNotFoundException {
 		switch(type) {
 		
-		case "css": return returnCSS(name); 
-		case "img": return returnImage(name);
-		case "javascript": return returnJavascript(name);
+		case "css": return getCSS(name); 
+		case "img": return getImage(name);
+		case "javascript": return getJavascript(name);
+		case "html": return getHTML(name);
 		default: return null;
 		}
 	}
 	
 	@Produces("text/css")
-	private InputStream returnCSS(String resource) throws FileNotFoundException {		
-		return new FileInputStream(new File("WebContent\\CSS\\" + resource)); 	
+	private static InputStream getCSS(String resource) throws FileNotFoundException {		
+		return new FileInputStream(new File("WebContent\\CSS\\" + resource));
+		//return new FileInputStream(new File("WebContent/CSS/" + resource));
 	}
 	
 	@Produces("image")
-	private InputStream returnImage(String resource) throws FileNotFoundException {		
-		return new FileInputStream(new File("WebContent\\Images\\" + resource)); 	
+	private static InputStream getImage(String resource) throws FileNotFoundException {		
+		return new FileInputStream(new File("WebContent\\Images\\" + resource));
+		//return new FileInputStream(new File("WebContent/Images/" + resource));
 	}
 	
 	@Produces("text/javascript")
-	private InputStream returnJavascript(String resource) throws FileNotFoundException {		
-		return new FileInputStream(new File("WebContent\\Javascript\\" + resource)); 	
+	private static InputStream getJavascript(String resource) throws FileNotFoundException {		
+		return new FileInputStream(new File("WebContent\\Javascript\\" + resource));
+		//return new FileInputStream(new File("WebContent/JavaScript/" + resource));
+	}
+	
+	@Produces("text/html")
+	private static InputStream getHTML(String resource) throws FileNotFoundException {		
+		return new FileInputStream(new File("WebContent\\HTML\\" + resource));
+		//return new FileInputStream(new File("WebContent/HTML/" + resource));
 	}
 	
 }
